fec60c6b77f1dd53d33e715db68b39502bf1b99b
matthewf <matthew.funcke@derivco.co.za>
2018-10-23 14:24:32 -0400

    Started adding concrete layout classes.

---
+++ GASeatingProblem/Seating/LayoutChromosome.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GASeatingProblem.Seating
+{
+    class LayoutChromosome : IChromosome<int>
+    {
+        readonly List<int> personIDs = new List<int>();
+        
+        public void InitialiseFromStrings(List<string> rawStrings)
+        {
+            //Each person gets a unique ID (corresponding to a lookup-array somewhere else).
+            personIDs.Clear();
+            foreach (var id in rawStrings)
+            {
+                personIDs.Add(int.Parse(id));
+            }
+        }
+
+        public Tuple<IChromosome<int>, IChromosome<int>> CrossoverWith(IChromosome<int> partner)
+        {
+            //TODO - take current class, and partner class, and make real offspring.
+            var child1 = new LayoutChromosome();
+            var child2 = new LayoutChromosome();
+
+            return new Tuple<IChromosome<int>, IChromosome<int>>(child1, child2);
+        }
+        
+        public void Mutate()
+        {
+            //TODO - change this to mutate more randomly, AND mutate in a way that can bring in people who have been excluded.
+
+            //A simple swap like this ensure that no duplication occurs.
+            var temp = personIDs[0];
+            personIDs[0] = personIDs[1];
+            personIDs[1] = temp;
+        }
+
+        public List<int> GetRawElements()
+        {
+            return personIDs;
+        }
+    }
+}
---
+++ GASeatingProblem/Seating/LayoutEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GASeatingProblem.Seating
+{
+    class LayoutEvaluator : ILayoutEvaluator<int>
+    {
+        public double GetFitness(IChromosome<int> chromosome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsValid(IChromosome<int> chromosome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitialiseFromFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
